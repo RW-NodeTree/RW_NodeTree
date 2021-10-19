c2c190dd5d7f80f4a55680840c9a30b6f3188241
Cn-mjt44 <1609605489@qq.com>
2021-10-20 00:32:20 +0800

    Basic comp

---
+++ RW_ModualWeapon/Comp_PartNode.cs
@@ -0,0 +1,163 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RW_ModularWeapon
+{
+    public class Comp_ThingsNode : ThingComp
+    {
+
+        public Comp_ThingsNode this[int index]
+        {
+            get
+            {
+                return childNodes[index];
+            }
+            set
+            {
+                if(AllowNode(value))
+                {
+                    childNodes[index] = value;
+                    UpdateNode();
+                }
+            }
+        }
+
+        public IEnumerable<ThingComp_BasicNodeComp> AllNodeComp
+        {
+            get
+            {
+                foreach (ThingComp_BasicNodeComp comp in parent.AllComps)
+                {
+                    if (comp != null)
+                    {
+                        yield return comp;
+                    }
+                }
+                yield break;
+            }
+        }
+
+        public bool AllowNode(Comp_ThingsNode node)
+        {
+            foreach (ThingComp_BasicNodeComp comp in AllNodeComp)
+            {
+                if (!comp.AllowNode(node)) return false;
+            }
+            return true;
+        }
+
+        public Comp_ThingsNode FindNode(Predicate<Comp_ThingsNode> func)
+        {
+            return childNodes.Find(func);
+        }
+
+        public void UpdateNode()
+        {
+            foreach(ThingComp_BasicNodeComp comp in AllNodeComp)
+            {
+                comp.UpdateNode();
+            }
+            foreach(Comp_ThingsNode node in childNodes)
+            {
+                node.UpdateNode();
+            }
+        }
+
+        public static implicit operator ThingWithComps(Comp_ThingsNode node)
+        {
+            return node.parent;
+        }
+
+        private bool onUpdateNode = false;
+
+        private Comp_ThingsNode parentNode = null;
+
+        private List<Comp_ThingsNode> childNodes = new List<Comp_ThingsNode>();
+    }
+
+    public class CompProperties_PartNode : CompProperties
+    {
+
+        /// <summary>
+        /// graphic data used when draw on parent node
+        /// </summary>
+        public GraphicData partIconGraphic = null;
+
+        /// <summary>
+        /// offset(+-) value for armor penetration
+        /// </summary>
+        public float armorPenetrationOffset = 0;
+
+        /// <summary>
+        /// offset(+-) value for melee weapon cooldown time
+        /// </summary>
+        public float meleeCooldownTimeOffset = 0;
+
+        /// <summary>
+        /// offset(+-) value for melee weapon damage
+        /// </summary>
+        public float meleeDamageOffset = 0;
+
+        /// <summary>
+        /// Multiplier value for ticks between burst shots,faster when value smaller
+        /// </summary>
+        public float ticksBetweenBurstShotsMultiplier = 1f;
+
+        /// <summary>
+        /// Multiplier value for muzzle flash scale,brighter when value biger
+        /// </summary>
+        public float muzzleFlashScaleMultiplier = 1f;
+
+        /// <summary>
+        /// Multiplier value for shooting rang,farther when value biger
+        /// </summary>
+        public float RangMultiplier = 1f;
+
+        /// <summary>
+        /// offset(+-) value for burst shot count
+        /// </summary>
+        public float burstShotCountOffset = 0;
+
+        /// <summary>
+        /// Multiplier value for shooting warmup time(the time need to take before whepon shoting),longer when value biger
+        /// </summary>
+        public float warmupTimeMultiplier = 1f;
+
+        /// <summary>
+        /// if is true,weapon can conly shoot one round when magazine capacity is 0
+        /// </summary>
+        public bool forceBrushShotWithOutMagCE = false;
+
+        /// <summary>
+        /// it set to not be affected by other part of parent node
+        /// </summary>
+        public bool verbPropertiesAffectByOtherPart = true;
+
+        /// <summary>
+        /// projectile replacement
+        /// </summary>
+        public ThingDef forcedProjectile = null;
+
+        /// <summary>
+        /// shooting sound replacement
+        /// </summary>
+        public SoundDef forceSound = null;
+
+        /// <summary>
+        /// Cast tail sound replacement
+        /// </summary>
+        public SoundDef forceSoundCastTail = null;
+
+
+        public List<StatModifier> statMultiplier = new List<StatModifier>();
+
+
+        public List<StatModifier> statOffset = new List<StatModifier>();
+    }
+}
---
+++ RW_ModualWeapon/ThingComp_BasicNodeComp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RW_ModularWeapon
+{
+    public abstract class ThingComp_BasicNodeComp:ThingComp
+    {
+        public abstract void UpdateNode();
+        public abstract bool AllowNode(Comp_ThingsNode node);
+
+    }
+}
