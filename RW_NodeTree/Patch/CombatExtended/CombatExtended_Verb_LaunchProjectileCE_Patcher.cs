768e4151882ff026b1e84ce8ce6ea12925755624
Future <1609605489@qq.com>
2022-11-12 15:50:52 +0800

    new patch for ce

---
+++ RW_NodeTree/Patch/CombatExtended/CombatExtended_Verb_LaunchProjectileCE_Patcher.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RW_NodeTree.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RW_NodeTree.Patch.CombatExtended
+{
+    internal static class CombatExtended_Verb_LaunchProjectileCE_Patcher
+    {
+        private static MethodInfo _PostPostVerb_LaunchProjectileCE_ShotsPerBurst = typeof(CombatExtended_Verb_LaunchProjectileCE_Patcher).GetMethod("PostVerb_LaunchProjectileCE_ShotsPerBurst", BindingFlags.Static | BindingFlags.NonPublic);
+        private static Type CombatExtended_Verb_LaunchProjectileCE = GenTypes.GetTypeInAnyAssembly("CombatExtended.Verb_LaunchProjectileCE");
+
+        private static void PostVerb_LaunchProjectileCE_ShotsPerBurst(Verb __instance, ref int __result)
+        {
+            CompChildNodeProccesser comp = (__instance.verbTracker.directOwner as ThingComp)?.parent;
+            if (comp != null)
+            {
+                __result = __instance.verbProps.burstShotCount;
+            }
+        }
+
+
+        public static void PatchVerb_LaunchProjectileCE(Harmony patcher)
+        {
+            if (CombatExtended_Verb_LaunchProjectileCE != null)
+            {
+                MethodInfo target = CombatExtended_Verb_LaunchProjectileCE.GetMethod("get_ShotsPerBurst", BindingFlags.Instance | BindingFlags.Public);
+                patcher.Patch(
+                    target,
+                    postfix: new HarmonyMethod(_PostPostVerb_LaunchProjectileCE_ShotsPerBurst)
+                    );
+            }
+        }
+    }
+}
