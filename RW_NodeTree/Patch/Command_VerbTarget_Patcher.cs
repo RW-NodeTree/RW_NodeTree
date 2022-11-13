6b621a471a48a42dfff23a104dc5fa7803f01494
Future <1609605489@qq.com>
2022-11-13 12:38:51 +0800

    Command_VerbTarget_Patcher

---
+++ RW_NodeTree/Patch/Command_VerbTarget_Patcher.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RW_NodeTree.Patch
+{
+    [HarmonyPatch(typeof(Command_VerbTarget))]
+    internal static class Command_VerbTarget_Patcher
+    {
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(
+            typeof(Command_VerbTarget),
+            "get_IconDrawColor"
+        )]
+        private static bool PreVerb_DirectOwner(Command_VerbTarget __instance, ref Color __result)
+        {
+            CompChildNodeProccesser proccess = ((__instance.verb?.verbTracker.directOwner as ThingComp)?.parent) ?? ((__instance.verb?.verbTracker.directOwner) as Thing);
+            if(proccess != null)
+            {
+                Thing part = proccess.GetBeforeConvertVerbCorrespondingThing(__instance.verb.verbTracker.directOwner.GetType(), __instance.verb, proccess.Props.VerbIconVerbInstanceSource).Item1;
+                if(part != null)
+                {
+                    __result = part.DrawColor;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
