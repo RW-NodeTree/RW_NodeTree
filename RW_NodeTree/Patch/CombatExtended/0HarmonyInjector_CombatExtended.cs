c64df85e46840cc7b7853ed9c521770436c0a866
Future <1609605489@qq.com>
2022-11-13 11:31:04 +0800

    CombatExtended_LoadoutPropertiesExtension_Patcher

--- RW_NodeTree/Patch/CombatExtended/0HarmonyInjector_CombatExtended.cs
+++ RW_NodeTree/Patch/CombatExtended/0HarmonyInjector_CombatExtended.cs
@@ -18,6 +18,7 @@ namespace RW_NodeTree.Patch.CombatExtended
             CombatExtended_CompFireModes_Patcher.PatchVerb(patcher);
             CombatExtended_JobDriver_Reload_Patcher.PatchJobDriver_Reload(patcher);
             CombatExtended_Verb_LaunchProjectileCE_Patcher.PatchVerb_LaunchProjectileCE(patcher);
+            CombatExtended_LoadoutPropertiesExtension_Patcher.PatchLoadoutPropertiesExtension(patcher);
         }
 
         public static Harmony patcher = new Harmony("RW_NodeTree.Patch.CombatExtended");
---
+++ RW_NodeTree/Patch/CombatExtended/CombatExtended_LoadoutPropertiesExtension_Patcher.cs
@@ -0,0 +1,90 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RW_NodeTree.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RW_NodeTree.Patch.CombatExtended
+{
+    internal static class CombatExtended_LoadoutPropertiesExtension_Patcher
+    {
+        private static MethodInfo _LoadoutPropertiesExtension_LoadWeaponWithRandAmmo_Transpiler = typeof(CombatExtended_LoadoutPropertiesExtension_Patcher).GetMethod("LoadoutPropertiesExtension_LoadWeaponWithRandAmmo_Transpiler", BindingFlags.Static | BindingFlags.NonPublic);
+        private static MethodInfo _LoadoutPropertiesExtension_TryGenerateAmmoFor_Transpiler = typeof(CombatExtended_LoadoutPropertiesExtension_Patcher).GetMethod("LoadoutPropertiesExtension_TryGenerateAmmoFor_Transpiler", BindingFlags.Static | BindingFlags.NonPublic);
+        private static MethodInfo _LoadoutPropertiesExtension_TryGetComp = typeof(CombatExtended_LoadoutPropertiesExtension_Patcher).GetMethod("LoadoutPropertiesExtension_TryGetComp", BindingFlags.Static | BindingFlags.NonPublic);
+        private static Type CombatExtended_LoadoutPropertiesExtension = GenTypes.GetTypeInAnyAssembly("CombatExtended.LoadoutPropertiesExtension");
+        private static Type CombatExtended_CompAmmoUser = GenTypes.GetTypeInAnyAssembly("CombatExtended.CompAmmoUser");
+
+        private static MethodInfo TryGetComp;
+
+
+        private static IEnumerable<CodeInstruction> LoadoutPropertiesExtension_LoadWeaponWithRandAmmo_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.Calls(TryGetComp)) yield return new CodeInstruction(OpCodes.Call, _LoadoutPropertiesExtension_TryGetComp);
+                else yield return instruction;
+            }
+        }
+
+        private static IEnumerable<CodeInstruction> LoadoutPropertiesExtension_TryGenerateAmmoFor_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.Calls(TryGetComp)) yield return new CodeInstruction(OpCodes.Call, _LoadoutPropertiesExtension_TryGetComp);
+                else yield return instruction;
+            }
+        }
+
+        private static ThingComp LoadoutPropertiesExtension_TryGetComp(Thing thing)
+        {
+            CompChildNodeProccesser comp = thing;
+            ThingComp result = null;
+            if (comp != null)
+            {
+                CompEquippable equippable = thing.TryGetComp<CompEquippable>();
+                if (equippable != null)
+                {
+                    thing = comp.GetBeforeConvertVerbCorrespondingThing(equippable.GetType(), equippable.PrimaryVerb, true).Item1 as ThingWithComps;
+
+                    result = (ThingComp)TryGetComp.Invoke(null, new object[] { thing });
+                    if (result != null) return result;
+
+                    thing = comp.GetBeforeConvertVerbCorrespondingThing(equippable.GetType(), equippable.PrimaryVerb).Item1 as ThingWithComps;
+
+                    result = (ThingComp)TryGetComp.Invoke(null, new object[] { thing });
+                    if (result != null) return result;
+                }
+            }
+            return result;
+        }
+
+
+        public static void PatchLoadoutPropertiesExtension(Harmony patcher)
+        {
+            if(CombatExtended_CompAmmoUser != null)
+            {
+                TryGetComp = typeof(ThingCompUtility).GetMethod("TryGetComp", BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(new Type[] { CombatExtended_CompAmmoUser });
+            }
+            if (CombatExtended_LoadoutPropertiesExtension != null)
+            {
+                MethodInfo target = CombatExtended_LoadoutPropertiesExtension.GetMethod("LoadWeaponWithRandAmmo", BindingFlags.Instance | BindingFlags.NonPublic);
+                patcher.Patch(
+                    target,
+                    transpiler: new HarmonyMethod(_LoadoutPropertiesExtension_LoadWeaponWithRandAmmo_Transpiler)
+                    );
+                target = CombatExtended_LoadoutPropertiesExtension.GetMethod("TryGenerateAmmoFor", BindingFlags.Instance | BindingFlags.NonPublic);
+                patcher.Patch(
+                    target,
+                    transpiler: new HarmonyMethod(_LoadoutPropertiesExtension_TryGenerateAmmoFor_Transpiler)
+                    );
+            }
+        }
+    }
+}
