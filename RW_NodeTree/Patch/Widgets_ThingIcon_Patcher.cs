41273f7bf85ff60359cc8f693bace8079a52705e
Future <1609605489@qq.com>
2022-11-18 14:56:46 +0800

    update

--- RW_NodeTree/Patch/Widgets_ThingIcon_Patcher.cs
+++ RW_NodeTree/Patch/Widgets_ThingIcon_Patcher.cs
@@ -22,12 +22,11 @@ namespace RW_NodeTree.Patch
             typeof(Thing),
             typeof(float),
             typeof(Rot4?)
-#if V13
-#else
+#if !V13
             ,typeof(bool)
 #endif
         )]
-        private static void PreWidgets_ThingIcon(Thing thing, ref Vector2? __state)
+        private static void PreWidgets_ThingIcon(Thing thing, ref (Vector2, float)? __state)
         {
             ThingStyleDef styleDef = thing.StyleDef;
             if (thing?.def?.graphicData != null && (styleDef == null || styleDef.UIIcon == null) && thing.def.uiIconPath.NullOrEmpty() && !(thing is Pawn || thing is Corpse))
@@ -37,9 +36,20 @@ namespace RW_NodeTree.Patch
                 {
                     Rot4 defaultPlacingRot = thing.def.defaultPlacingRot;
                     ref Vector2 drawSize = ref thing.def.graphicData.drawSize;
-                    __state = drawSize;
+                    ref float scale = ref thing.def.uiIconScale;
+                    __state = (drawSize, scale);
                     drawSize = proccesser.GetAndUpdateDrawSize(defaultPlacingRot);
-                    if (defaultPlacingRot.IsHorizontal) drawSize = drawSize.Rotated();
+                    if (defaultPlacingRot.IsHorizontal)
+                    {
+                        drawSize = drawSize.Rotated();
+                        Vector2 scaleCalc = drawSize / thing.def.size.ToVector2().Rotated();
+                        scale *= Math.Max(scaleCalc.x, scaleCalc.y);
+                    }
+                    else
+                    {
+                        Vector2 scaleCalc = drawSize / thing.def.size.ToVector2();
+                        scale *= Math.Max(scaleCalc.x, scaleCalc.y);
+                    }
                     //Log.Message($"thing.def.graphicData.drawSize : {drawSize}; cache : {__state}");
                 }
             }
@@ -54,16 +64,16 @@ namespace RW_NodeTree.Patch
             typeof(Thing),
             typeof(float),
             typeof(Rot4?)
-#if V13
-#else
+#if !V13
             ,typeof(bool)
 #endif
         )]
-        private static void PostWidgets_ThingIcon(Thing thing, ref Vector2? __state)
+        private static void PostWidgets_ThingIcon(Thing thing, (Vector2, float)? __state)
         {
             if (thing?.def?.graphicData != null && __state.HasValue)
             {
-                thing.def.graphicData.drawSize = __state.Value;
+                thing.def.graphicData.drawSize = __state.Value.Item1;
+                thing.def.uiIconScale = __state.Value.Item2;
             }
         }
     }
