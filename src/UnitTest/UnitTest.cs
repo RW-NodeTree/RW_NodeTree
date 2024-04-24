3fb66ba8e5f53f456966387d62eb88615575d730
Future <1609605489@qq.com>
2024-04-24 10:08:49 +0800

    node query

---
+++ src/UnitTest/UnitTest.cs
@@ -0,0 +1,14 @@
+using RW_NodeTree;
+
+namespace UnitTest;
+
+[TestClass]
+public class UnitTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        QueryGroup obj = new QueryGroup("[]/hasf[!ggg/afx:asd,dsd][wgw:aa],hgerg:afs");
+        Console.WriteLine(obj);
+    }
+}
